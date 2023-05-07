0a1cb1d84cd356e27f76faea0586a35796b3c610
JS2C <juansecas1997@gmail.com>
2023-05-06 23:39:59 -0500

    Nav, toolbar, wrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './modules/angular-material/angular-material.module';
+import { NavegatorComponent } from './shared/components/navegator/navegator.component';
+import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
+import { WrapComponent } from './shared/components/wrap/wrap.component';
+import { UsuariosComponent } from './utilities/components/usuarios/usuarios.component';
+import { MateriasComponent } from './utilities/components/materias/materias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegatorComponent,
+    ToolbarComponent,
+    WrapComponent,
+    UsuariosComponent,
+    MateriasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
